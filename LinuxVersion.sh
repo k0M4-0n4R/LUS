478a4a0428acf55120432bdd42519e16554d0934
k0M4-0n4R <97519662+k0M4-0n4R@users.noreply.github.com>
2022-05-05 10:11:17 +0200

    Update LinuxVersion.sh

--- LinuxVersion.sh
+++ LinuxVersion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This is an easy Script to Upgrade your Linux
+#You must only one time press "Y" to accept the Upgrade
 #Go and take a pot of Coffee ;)  
 
 echo “Upgrade auf eine neue Linux Version“
@@ -8,5 +9,7 @@ sudo chmod +x LinuxVersion.sh
 do-release-upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get auotremove -y
+sudo apt-get clean -y
 do-release-upgrade -d -y
 do-release-upgrade
